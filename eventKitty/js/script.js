83924bd10bb0402e94ae158f5d6575569f797aaf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-06-27 14:59:09 -0400

    Solution code for event kitty pt 1

---
+++ eventKitty/js/script.js
@@ -0,0 +1,8 @@
+
+
+let button = document.querySelector('.show-cat');
+let cat = document.querySelector('.cat');
+button.addEventListener('click', () => {
+  cat.classList.add('show');
+  //console.log('adsfsfa');
+});
